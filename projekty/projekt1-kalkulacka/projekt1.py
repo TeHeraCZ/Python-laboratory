db74d52c7d148844c3a7a4b996132e577d9cd438
TeHeraCZ <95979205+TeHeraCZ@users.noreply.github.com>
2021-12-28 11:15:16 +0100

    progress

---
+++ projekty/projekt1-kalkulacka/projekt1.py
@@ -0,0 +1,15 @@
+number1 = int(input("napis cislo 1:"))
+number2 = int(input("napis cislo 2:"))
+operace = input("jakou operaci chces udelat [+ - * /]")
+
+if operace == "+":
+    print(number1+number2)
+
+if operace == "-":
+    print(number1-number2)
+
+if operace == "*":
+    print(number1*number2)
+
+if operace == "/":
+    print(number1/number2)
